6d03642035ff380843fae96ce3555f20ad362377
Dharshu114 <dharshd114@gmail.com>
2026-02-18 16:18:49 +0530

    Create problem 36.c

---
+++ problem 36.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    float height;
+
+    printf("Enter height in centimeters: ");
+    scanf("%f", &height);
+
+    if (height < 150)
+    {
+        printf("Category: Short");
+    }
+    else if (height >= 150 && height < 165)
+    {
+        printf("Category: Average Height");
+    }
+    else if (height >= 165 && height < 180)
+    {
+        printf("Category: Tall");
+    }
+    else
+    {
+        printf("Category: Very Tall");
+    }
+
+    return 0;
+}
