36d4c7d74bd7a3622093431e765c8566651be0de
Dharshu114 <dharshd114@gmail.com>
2026-02-18 15:13:29 +0530

    Create problem 21.c

---
+++ problem 21.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    float temp;
+
+    printf("Enter temperature in Centigrade: ");
+    scanf("%f", &temp);
+
+    if (temp <= 0)
+    {
+        printf("Freezing weather\n");
+    }
+    else if (temp > 0 && temp <= 10)
+    {
+        printf("Very Cold weather\n");
+    }
+    else if (temp > 10 && temp <= 20)
+    {
+        printf("Cold weather\n");
+    }
+    else if (temp > 20 && temp <= 30)
+    {
+        printf("Normal temperature\n");
+    }
+    else if (temp > 30 && temp <= 40)
+    {
+        printf("Hot weather\n");
+    }
+    else
+    {
+        printf("Very Hot weather\n");
+    }
+
+    return 0;
+}
