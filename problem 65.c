5beb00748ae804be81c5ccdb1817296117c75f88
Dharshini <dharshd114@gmail.com>
2026-02-28 16:19:39 +0530

    Create problem 65.c

---
+++ problem 65.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    float x1, y1, x2, y2, slope;
+
+    scanf("%f %f %f %f", &x1, &y1, &x2, &y2);
+
+    if (x2 - x1 == 0)
+    {
+        printf("Slope is undefined");
+    }
+    else
+    {
+        slope = (y2 - y1) / (x2 - x1);
+        printf("Slope: %.2f", slope);
+    }
+
+    return 0;
+}
